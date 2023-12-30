738ecb5a4629ef78167f69ce185fe5bf51468929
Roger Caminal Armadans <roger.caminal@chartboost.com>
2023-12-30 19:11:03 +0100

    Lint

--- src/hamcontestanalysis/plots/common/plot_log_heatmap.py
+++ src/hamcontestanalysis/plots/common/plot_log_heatmap.py
@@ -1,15 +1,17 @@
 """Plot QSO rate."""
 
-from plotly.subplots import make_subplots
+from typing import List
+
 import plotly.graph_objects as go
 import plotly.offline as pyo
-from numpy import floor, arange, vectorize, where
+from numpy import arange
+from numpy import floor
+from numpy import vectorize
+from numpy import where
 from pandas import DataFrame
 from pandas import Grouper
-from pandas import merge
-from pandas import to_datetime
-from pandas import to_timedelta
 from pandas import pivot_table
+from plotly.subplots import make_subplots
 
 from hamcontestanalysis.plots.plot_base import PlotBase
 from hamcontestanalysis.utils import CONTINENTS
@@ -24,7 +26,7 @@ class PlotLogHeatmap(PlotBase):
         mode: str,
         callsigns_years: list[tuple[str, int]],
         time_bin_size: int = 1,
-        continents: list[str] | None = None,
+        continents: List[str] | None = None,
     ):
         """Init method of the PlotLogHeatmap class.
 
@@ -33,6 +35,8 @@ class PlotLogHeatmap(PlotBase):
             mode (str): Mode of the contest
             callsigns_years (list[tuple[str, int]]): Callsign and year of the contest
             time_bin_size (int, optional): Time bin size in minutes. Defaults to 1.
+            continents (List[str] | None, optional): Continents to consider. Defaults
+                to None.
         """
         super().__init__(contest=contest, mode=mode, callsigns_years=callsigns_years)
         self.time_bin = time_bin_size
@@ -42,8 +46,7 @@ class PlotLogHeatmap(PlotBase):
         """Prepare dataframe for plotting."""
         # Aggregate original dataframe
         grp = (
-            self.data
-            .query(f"continent.isin({self.continents})")
+            self.data.query(f"continent.isin({self.continents})")
             .assign(
                 callsign_year=lambda x: x["mycall"] + "(" + x["year"].astype(str) + ")",
                 contest_hour=lambda x: floor(x["hour"]).astype(int),
@@ -52,36 +55,34 @@ class PlotLogHeatmap(PlotBase):
                 [
                     Grouper(key="datetime", freq=f"{self.time_bin}Min"),
                     "callsign_year",
-                    "contest_hour"
+                    "contest_hour",
                 ],
                 as_index=False,
             )
-            .agg(
-                qsos=("callsign_year", "count"),
-                calls=("call", "unique")
-            )
+            .agg(qsos=("callsign_year", "count"), calls=("call", "unique"))
             .assign(
                 contest_minute=lambda x: x["datetime"].dt.minute.astype(int),
             )
         )
 
         # Template for missing values
-        df_minutes = DataFrame(arange(0, 60, self.time_bin), columns=["contest_minute"]).astype({"contest_minute": int})
-        df_hours = DataFrame(arange(0, 48, self.time_bin), columns=["contest_hour"]).astype({"contest_hour": int})
-        df_call_year = DataFrame(grp["callsign_year"].unique(), columns=["callsign_year"])
-        df_template = (
-            df_hours
-            .merge(df_minutes, how='cross')
-            .merge(df_call_year, how='cross')
+        df_minutes = DataFrame(
+            arange(0, 60, self.time_bin), columns=["contest_minute"]
+        ).astype({"contest_minute": int})
+        df_hours = DataFrame(
+            arange(0, 48, self.time_bin), columns=["contest_hour"]
+        ).astype({"contest_hour": int})
+        df_call_year = DataFrame(
+            grp["callsign_year"].unique(), columns=["callsign_year"]
+        )
+        df_template = df_hours.merge(df_minutes, how="cross").merge(
+            df_call_year, how="cross"
         )
 
         # Merge template with aggregated dataframe
         grp = (
-            df_template
-            .merge(
-                grp,
-                how="left",
-                on=["contest_minute", "contest_hour", "callsign_year"]
+            df_template.merge(
+                grp, how="left", on=["contest_minute", "contest_hour", "callsign_year"]
             )
             .drop(columns=["datetime"])
             .assign(
@@ -103,17 +104,27 @@ class PlotLogHeatmap(PlotBase):
         """
         n_callsigns_years = len(self.callsigns_years)
         fig = make_subplots(
-            rows=n_callsigns_years, 
-            cols=1, 
-            subplot_titles=[f"{callsign} ({year})" for callsign, year in self.callsigns_years]
+            rows=n_callsigns_years,
+            cols=1,
+            subplot_titles=[
+                f"{callsign} ({year})" for callsign, year in self.callsigns_years
+            ],
         )
         _data = self._prepare_dataframe()
         z_max = _data["qsos"].max()
         for irow, (callsign, year) in enumerate(self.callsigns_years):
             _df = _data.query(f"(callsign_year == '{callsign.upper()}({year})')")
-            table = pivot_table(_df, values="qsos", index=["contest_hour"], columns=["contest_minute"]) 
+            table = pivot_table(
+                _df, values="qsos", index=["contest_hour"], columns=["contest_minute"]
+            )
             # import pdb; pdb.set_trace()
-            table_calls = pivot_table(_df, values="calls", index=["contest_hour"], columns=["contest_minute"], aggfunc=lambda x: x.values[0])
+            table_calls = pivot_table(
+                _df,
+                values="calls",
+                index=["contest_hour"],
+                columns=["contest_minute"],
+                aggfunc=lambda x: x.values[0],
+            )
             vfunc = vectorize(lambda x: f"QSOs: {', '.join(x)}")
 
             fig.add_trace(
@@ -127,18 +138,17 @@ class PlotLogHeatmap(PlotBase):
                         title=f"QSOs / {self.time_bin}min",
                         titleside="top",
                         tickmode="array",
-                        tickvals=arange(0, z_max+1, 1),
-                        ticks="outside"
-                    )
+                        tickvals=arange(0, z_max + 1, 1),
+                        ticks="outside",
+                    ),
                 ),
-                row=irow+1,
-                col=1
+                row=irow + 1,
+                col=1,
             )
 
         fig.update_layout(hovermode="x unified")
         fig.update_xaxes(title="Contest minute")
         fig.update_yaxes(title="Contest hour")
-
 
         if not save:
             return fig
--- tests/plots/common/test_plot_log_heatmap.py
+++ tests/plots/common/test_plot_log_heatmap.py
@@ -1,26 +1,36 @@
 """Test contest log heatmap plot."""
+import pytest
 from pandas import read_parquet
 from pandas.testing import assert_frame_equal
-import pytest
+
 from hamcontestanalysis.plots.common.plot_log_heatmap import PlotLogHeatmap
 from hamcontestanalysis.utils import CONTINENTS
 
 
 @pytest.fixture
 def input_data():
-    return read_parquet("tests/resources/plots/common/plot_base__get_inputs__ef6t_2022_cr6k_2022.parquet")
+    return read_parquet(
+        "tests/resources/plots/common/plot_base__get_inputs__ef6t_2022_cr6k_2022.parquet"
+    )
+
 
 @pytest.fixture
 def validation_data():
-    return read_parquet("tests/resources/plots/common/plot_log_heatmap__prepare_dataframe__ef6t_2022_cr6k_2022.parquet")
+    return read_parquet(
+        "tests/resources/plots/common/plot_log_heatmap__prepare_dataframe__ef6t_2022_cr6k_2022.parquet"
+    )
+
 
 def test_class_arguments():
-    plot = PlotLogHeatmap(contest="cqww", mode="cw", callsigns_years=[("EF6T", 2022), ("CR6K", 2022)])
+    plot = PlotLogHeatmap(
+        contest="cqww", mode="cw", callsigns_years=[("EF6T", 2022), ("CR6K", 2022)]
+    )
     assert plot.contest == "cqww"
     assert plot.mode == "cw"
     assert plot.callsigns_years == [("EF6T", 2022), ("CR6K", 2022)]
     assert plot.time_bin == 1
     assert plot.continents == CONTINENTS
+
 
 def test_wrong_arguments():
     with pytest.raises(TypeError):
@@ -32,8 +42,13 @@ def test_wrong_arguments():
 
 
 def test_prepare_dataframe(mocker, input_data, validation_data):
-    mocker_get_inputs = mocker.patch("hamcontestanalysis.plots.plot_base.PlotBase._get_inputs", return_value=input_data)
-    plot = PlotLogHeatmap(contest="cqww", mode="cw", callsigns_years=[("EF6T", 2022), ("CR6K", 2022)])
+    mocker_get_inputs = mocker.patch(
+        "hamcontestanalysis.plots.plot_base.PlotBase._get_inputs",
+        return_value=input_data,
+    )
+    plot = PlotLogHeatmap(
+        contest="cqww", mode="cw", callsigns_years=[("EF6T", 2022), ("CR6K", 2022)]
+    )
     df_test = plot._prepare_dataframe()
     assert mocker_get_inputs.call_count == 1
     assert_frame_equal(df_test.astype({"calls": str}), validation_data)
